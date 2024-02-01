a83ed53a77fc80ade1bb72dd28f9244a266714cf
tianzq13184 <128171853+tianzq13184@users.noreply.github.com>
2024-02-01 17:22:46 -0500

    album cover

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -5,13 +5,24 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
@@ -27,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AlbumCover()
                 }
             }
         }
@@ -69,7 +80,46 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            AlbumCover()
         }
+    }
+}
+
+@Composable
+private fun LoadingSection(text: String) {
+    Row(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+private fun AlbumCover() {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = "Still Fantasy",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 4.dp, start = 2.dp)
+                    .align(Alignment.BottomStart)
+            )
+        }
+
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray,
+        )
     }
 }
