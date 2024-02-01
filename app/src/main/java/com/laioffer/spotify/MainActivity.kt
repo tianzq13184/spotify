4e0fabbededaf7b4b415f7dda16ae8a2fce13199
tianzq13184 <128171853+tianzq13184@users.noreply.github.com>
2024-02-01 16:00:15 -0500

    change main activity

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -15,8 +16,10 @@ import com.laioffer.spotify.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
+    private val TAG = "lifecycle"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "We are at onCreate()")
         setContent {
             SpotifyTheme {
                 // A surface container using the 'background' color from the theme
@@ -29,6 +32,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "We are at onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "We are at onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "We are at onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "We are at onStop()")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "We are at onDestroy()")
+        super.onDestroy()
+    }
 }
 
 @Composable
@@ -36,7 +64,7 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 412, heightDp = 732)
 @Composable
 fun DefaultPreview() {
     SpotifyTheme {
